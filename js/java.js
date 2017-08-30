aba1ca66af0ead2c3d4efd1274dec59f8f116685
Russel Crow <pyccok@gmail.com>
2017-08-30 19:58:56 +0300

    site_prog

--- js/java.js
+++ js/java.js
@@ -1,6 +1,9 @@
 $(".nav_menu span").click(function(){
+	$(".menu_open").addClass("menu_open_active")
 	$(".menu_center_click").slideDown(1000)
-	$(".menu_open_click").click(function(){
-	$(".menu_center_click").slideUp(300)
+	$(".menu_open_active").click(function(){
+		$(".menu_open_active").removeClass("menu_open_active")
+		$(".menu_center_click").slideUp(500)
 	});
+	
 });
