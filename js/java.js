d02b1d0fe102aa7df3992ca4566b548d4f06b0e6
Russel Crow <pyccok@gmail.com>
2017-09-02 10:21:45 +0300

    site_prog

--- js/java.js
+++ js/java.js
@@ -7,3 +7,39 @@ $(".nav_menu span").click(function(){
 	});
 	
 });
+
+
+
+function get_random_color() {
+   var letters = '0123456789ABCDEF'.split('');
+   var color = '#';
+    for (var i = 0; i < 3; i++ ) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
+}
+
+
+$(document).ready(function() {
+	var block = ".main_center_block_info";
+	var border_content = ".main_center_block_info .header_block";
+
+	$(block).hover(function(){
+		$(border_content).css({"border-color" : get_random_color()})
+			$(border_content).hover(function() {
+				$(border_content).css({"border-color" : get_random_color()})
+			});
+	});
+});
+
+$(document).ready(function () {
+	 $('input,textarea').focus(function(){
+	   $(this).data('placeholder',$(this).attr('placeholder'))
+	   $(this).attr('placeholder','');
+	 });
+	 $('input,textarea').blur(function(){
+	   $(this).attr('placeholder',$(this).data('placeholder'));
+ });
+ });
+
+
